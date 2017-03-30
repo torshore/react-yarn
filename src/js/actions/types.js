0462377bedb02fe5a060b0439eb27bc4e109579d
Macky Beltran <mackybeltran1@gmail.com>
2017-03-30 19:40:36 -0400

    choice component showing with no styling

--- src/js/actions/types.js
+++ src/js/actions/types.js
@@ -1,5 +1,6 @@
 export const GET_STORIES = "GET_STORIES";
 export const GET_PANEL = "GET_PANEL";
 export const CREATE_STORIES = "CREATE_STORIES";
+export const GET_CHOICES = "GET_CHOICES";
 export const CREATE_STORY = "CREATE_STORY";
 export const DELETE_STORY = "DELETE_STORY";
