14f1ae63ddddd6b297189df7b1ef4be502948e1d
Macky Beltran <mackybeltran1@gmail.com>
2017-04-03 12:34:21 -0400

    added new story form. it is now sending data to /stories in the backend

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import SingleStoryShow from './js/components/SingleStoryShow';
 import StoriesHome from './js/components/StoriesHome';
 import PanelEdit from './js/components/PanelEdit';
 import StoryChart from './js/components/StoryChart'
+import NewStory from './js/components/NewStory'
 import store, { history } from './store.js';
 import 'react-materialize';
 
@@ -16,9 +17,11 @@ const router = (
    <BrowserRouter history={history}>
       <App>
           <Route exact={true} path="/" />
+          <Route path="/new" component={NewStory}/>
           <Switch>
             <Route path="/stories/:storyid/panels/:panelid" component={SingleStoryShow}/>
             <Route path="/stories/:storyid/" component={StoryChart}/>
+
             <Route path="/stories" component={StoriesHome} />
           </Switch>
           <Route path="/stories/:storyid/panels/:panelid/edit" component={PanelEdit}/>
