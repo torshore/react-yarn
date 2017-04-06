7cc9cffffb5095e15f6b3c177a695cab33abc8c6
torshore <toryshoreman@gmail.com>
2017-04-05 23:43:13 -0400

    fixed some merge errors

--- src/js/components/StoryChart.js
+++ src/js/components/StoryChart.js
@@ -16,16 +16,6 @@ class StoryChart extends Component {
     this.props.dispatch(getPanels(this.props.match.params.storyid));
   }
 
-   getPanelData = (dataFromDb) => {
-      let result = dataFromDb.map((row, index) => {
-      var array = [];
-      array.push(row.id, row.panel_id, row.path_to, row.panel_title, row.index, row.index2, row.story_id, row.image, row.panel_text)
-      return array
-    })
-    return result
-  }
-
-
   columnRow = (dataFromDb) => {
     let result = dataFromDb.map((row, index) => {
       var array = [];
@@ -38,67 +28,58 @@ class StoryChart extends Component {
 
   render() {
     let rowsData = this.columnRow(this.props.rows);
-    let panelData = this.columnRow(this.props.panels);
 
     if (rowsData.length === 0) {
       return <div />
     }
-    return (
-      <div>
-      <div>
-        <StoryEdit rows={rowsData}/>
-      </div>
-      <div>
-        <Row className="build">
-            <Col m={3}>
-              <h3 className="bldtitle2"> View Chapters: </h3>
-              <Modal
-                header='Chapter 1: The Beginning'
-                trigger={
-                  <Button waves='light'>Chapter 1 : The Beginning</Button>
-                }>
-                <p>The Beginning</p>
-              </Modal>
-            </Col>
-          </Row>
-       </div>
-          <div>
-            <Col m={8} className="chart">
-              <h3 className="bldtitle3">Story Paths:</h3>
-              <Chart
-                chartType="OrgChart"
-                rows={rowsData}
-                columns={[
-                  {
-                    type: 'string',
-                    label: 'Child',
-                  },
-                  {
-                    type: 'string',
-                    label: 'Parent'
-                  },
-                  {
-                    type: 'string',
-                    label: 'Tooltip'
-                  }
-              ]}
-                graph_id="OrgChart"
-                width={'50%'}
-                height={'50%'}
-                legend_toggle
-                chartEvents={this.chartEvents}
-              />
-            </Col>
-            <Col m={1}/>
-          </div>
 
-      </div>
+return (
+  <div>
+    <Row className="build">
+      <Col m={3}>
+        <h3 className="bldtitle2"> View Chapters: </h3>
+        <Modal
+          header='Chapter 1: The Beginning'
+          trigger={
+            <Button waves='light'>Chapter 1 : The Beginning</Button>
+          }>
+          <p>The Beginning</p>
+        </Modal>
+        <StoryEdit panels={this.props.panels}/>
+      </Col>
 
-    );
+      <Col m={8} className="chart">
+        <h3 className="bldtitle3">Story Paths:</h3>
+        <Chart
+          chartType="OrgChart"
+          rows={rowsData}
+          columns={[
+            {
+              type: 'string',
+              label: 'Child',
+            },
+            {
+              type: 'string',
+              label: 'Parent'
+            },
+            {
+              type: 'string',
+              label: 'Tooltip'
+            }
+        ]}
+          graph_id="OrgChart"
+          width={'50%'}
+          height={'50%'}
+          legend_toggle
+          chartEvents={this.chartEvents}
+        />
+      </Col>
+      <Col m={1}/>
+      </Row>
+    </div>
+  );
  }
 }
-
-
 
 
 
--- src/js/components/StoryEdit.js
+++ src/js/components/StoryEdit.js
@@ -3,20 +3,15 @@ import { Button } from 'react-materialize';
 import { Link } from 'react-router-dom';
 import NewPanelForm from './NewPanelForm';
 
-
-
-
 class StoryEdit extends Component {
-
-
 
 
   renderPanels() {
     return this.props.panels.map((panel, i) => {
       return (
         <div key={i}>
-          <Link to={`/stories/${panel.storyid}/panels/${panel.id}`} onClick={NewPanelForm}>
-              <Button />
+          <Link to={`/stories/${panel.story_id}/panels/${panel.id}/choices/edit`} onClick={NewPanelForm}>
+              <Button>{panel.title}</Button>
           </Link>
         </div>
       )
